d7ba0c92788b86e3c9335ef7721d1cd42e95dd41
Иду в темноту <83702050+going-hi@users.noreply.github.com>
2024-03-04 22:57:37 +0300

    add logger in main file

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+  const logger = new Logger();
   const PORT = configService.get('PORT');
   await app.listen(PORT);
+  logger.log(`Server started on port ${PORT}`, 'APP');
 }
 bootstrap();
