69871a4e4e11078791119db1a82da8cef0912bef
Иду в темноту <83702050+going-hi@users.noreply.github.com>
2024-03-06 18:27:59 +0300

    add config swagger and setup swagger doc

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { Logger } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
+import { SwaggerModule } from '@nestjs/swagger';
+import { configSwagger } from './configs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +13,8 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   const logger = new Logger();
   const PORT = configService.get('PORT');
+  const document = SwaggerModule.createDocument(app, configSwagger);
+  SwaggerModule.setup('api', app, document);
   await app.listen(PORT);
   logger.log(`Server started on port ${PORT}`, 'APP');
 }
