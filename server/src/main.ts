4e3159ac8e6344c86a1c41cdbd1bf825aa6b79f2
Иду в темноту <83702050+going-hi@users.noreply.github.com>
2024-03-06 18:10:40 +0300

    add modules, add cookie parser and  seetGlobalPrefix (api)

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { Logger } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
+  app.setGlobalPrefix('api');
   const configService = app.get(ConfigService);
   const logger = new Logger();
   const PORT = configService.get('PORT');
