d5bfe16202e885ee44fb2a136a927e6f4ac51b0c
Иду в темноту <83702050+going-hi@users.noreply.github.com>
2024-03-07 23:49:38 +0300

    update import user dto

--- server/src/modules/user/user.service.ts
+++ server/src/modules/user/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './schemas/user.schema';
 import { Model } from 'mongoose';
-import { CreateUserDto } from './dto/create-user.dto';
+import { CreateUserDto } from './dto';
 
 @Injectable()
 export class UserService {
