721ebe404abf843676a43459ed5df1e47d2e3a1f
Иду в темноту <83702050+going-hi@users.noreply.github.com>
2024-03-14 20:50:26 +0300

    update import and eslint fix

--- server/src/notification/telegram/inteface/telegram.interface.ts
+++ server/src/notification/telegram/inteface/telegram.interface.ts
@@ -1,12 +1,12 @@
-import { ModuleMetadata } from "@nestjs/common"
+import { ModuleMetadata } from '@nestjs/common';
 
 export interface ITelegramOptions {
-  chatId: string
-  token: string
+  chatId: string;
+  token: string;
 }
 
-export interface ITelegramModuleAsynOptions extends Pick<ModuleMetadata, 'imports'> {
-  useFactory:(...args: any[]) => Promise<ITelegramOptions> | ITelegramOptions
-
-  inject?:any[]
+export interface ITelegramModuleAsyncOptions
+  extends Pick<ModuleMetadata, 'imports'> {
+  useFactory: (...args: any[]) => Promise<ITelegramOptions> | ITelegramOptions;
+  inject?: any[];
 }
