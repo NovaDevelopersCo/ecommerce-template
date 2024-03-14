7a2b88041289be5f2aaff638291fd0acb4917bb2
Иду в темноту <83702050+going-hi@users.noreply.github.com>
2024-03-14 20:48:26 +0300

    eslint fix and delete if

--- server/src/configs/telegram.config.ts
+++ server/src/configs/telegram.config.ts
@@ -1,13 +1,12 @@
-import { ConfigService } from "@nestjs/config";
-import { ITelegramOptions } from "src/telegram/inteface/telegram.interface";
+import { ConfigService } from '@nestjs/config';
+import { ITelegramOptions } from 'src/notification/telegram/inteface/telegram.interface';
 
-export const getTelegramConfig = (configService:ConfigService):ITelegramOptions => {
-  const token = configService.get('TELEGRAM_TOKEN')
-  if(!token) {
-    throw new Error('Telegram token is undefined')
-  }
+export const getTelegramConfig = (
+  configService: ConfigService,
+): ITelegramOptions => {
+  const token = configService.get('TELEGRAM_TOKEN');
   return {
     token,
-    chatId: configService.get('CHAT_ID') ?? ''
-  }
-}
+    chatId: configService.get('CHAT_ID'),
+  };
+};
