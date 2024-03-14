f22398f143de55be4829e8aeb8db0af155c2d9f6
Иду в темноту <83702050+going-hi@users.noreply.github.com>
2024-03-14 21:22:21 +0300

    update service and add module

---
+++ server/src/notification/mail/mail.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { MailService } from './mail.service';
+import { MailerModule } from '@nestjs-modules/mailer';
+
+@Module({
+  imports: [MailerModule],
+  providers: [MailService],
+  exports: [MailService],
+})
+export class MailModule {}
--- server/src/notification/mail/mail.service.ts
+++ server/src/notification/mail/mail.service.ts
@@ -8,24 +8,23 @@ import { IMail } from './interfaces';
 export class MailService {
   constructor(
     private readonly mailerService: MailerService,
-    private configService: ConfigService
+    private configService: ConfigService,
   ) {}
 
-  async emailSend<T>(mailData:IMail<T>) {
-    const urlConfirmAddress = this.configService.get<string>(
-      'URL_CONFIRM_ADDRESS',
-    );
-
-    return this.mailerService.sendMail({
-      to: mailData.to,
-      subject: mailData.subject,
-      template: join(__dirname, '/../templates', mailData.template),
-      context: mailData.context
-    }).catch((e) => {
+  async emailSend<T>(mailData: IMail<T>) {
+    try {
+      await this.mailerService.sendMail({
+        to: mailData.to,
+        subject: mailData.subject,
+        template: join(__dirname, '/../templates', mailData.template),
+        context: mailData.context,
+      });
+    } catch (e) {
+      console.error(e);
       throw new HttpException(
         `Ошибка работы почты: ${JSON.stringify(e)}`,
         HttpStatus.UNPROCESSABLE_ENTITY,
       );
-    });
+    }
   }
 }
