fc61f435fea93a7e1e27ada233be5d9612f6d0ff
Иду в темноту <83702050+going-hi@users.noreply.github.com>
2024-03-14 21:30:44 +0300

    update import in user module

--- server/src/modules/user/user.module.ts
+++ server/src/modules/user/user.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schemas/user.schema';
+import { MailModule } from 'src/notification/mail/mail.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MailModule,
   ],
   providers: [UserService],
   exports: [UserService],
