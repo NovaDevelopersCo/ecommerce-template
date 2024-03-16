0ef1df22c9cee3db021fcad391ad989fca97fe21
Иду в темноту <83702050+going-hi@users.noreply.github.com>
2024-03-16 16:50:20 +0300

    update dto for swagger

--- server/src/modules/manufacturer/dto/create-manufacturer.dto.ts
+++ server/src/modules/manufacturer/dto/create-manufacturer.dto.ts
@@ -1,14 +1,30 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsOptional, IsUrl } from 'class-validator';
 
 export class CreateManufacturerDto {
+  @ApiProperty()
   @IsString()
   name: string;
 
+  @ApiProperty({ required: false })
   @IsOptional()
   @IsString()
   description: string;
 
+  @ApiProperty({
+    required: false,
+    description: 'Url link to website manufacturer',
+  })
   @IsOptional()
   @IsUrl()
   link: string;
+
+  // * File upload
+  @ApiProperty({
+    type: 'string',
+    format: 'binary',
+    required: false,
+    description: 'file type should be jpeg | png | jpg | webp',
+  })
+  logo?: unknown;
 }
--- server/src/modules/manufacturer/dto/update-manufacturer.dto.ts
+++ server/src/modules/manufacturer/dto/update-manufacturer.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { CreateManufacturerDto } from './create-manufacturer.dto';
 
 export class UpdateManufacturerDto extends PartialType(CreateManufacturerDto) {}
