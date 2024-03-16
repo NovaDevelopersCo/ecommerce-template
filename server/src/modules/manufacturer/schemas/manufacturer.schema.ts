2b309ddd51cecb96f573010b6a1f5d9e81d634b2
Иду в темноту <83702050+going-hi@users.noreply.github.com>
2024-03-16 16:50:38 +0300

    update model for swagger

--- server/src/modules/manufacturer/schemas/manufacturer.schema.ts
+++ server/src/modules/manufacturer/schemas/manufacturer.schema.ts
@@ -1,20 +1,30 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { ApiProperty } from '@nestjs/swagger';
 import { AbstractModel } from 'src/core/abstract/abstract.schema';
 
 @Schema()
 export class Manufacturer extends AbstractModel {
+  @ApiProperty()
   @Prop({ required: true })
   name: string;
 
+  @ApiProperty({ required: false })
   @Prop()
-  description: string;
+  description?: string;
 
-  // * Url link to website manufacturer
+  @ApiProperty({
+    required: false,
+    description: 'Url link to website manufacturer',
+  })
   @Prop()
-  link: string;
+  link?: string;
 
+  @ApiProperty({ required: false, description: 'Img link' })
   @Prop()
-  logo: string;
+  logo?: string;
+
+  // *
+  //* products:
 }
 
 export const ManufacturerSchema = SchemaFactory.createForClass(Manufacturer);
