cea5267632def380d912c93323e143549938b82b
vtesliuk <109725756+Lordpluha@users.noreply.github.com>
2024-03-21 10:59:51 +0100

    Add prettifying in server package + add global scripts for dev

--- client/src/app/(store)/catalog/page.tsx
+++ client/src/app/(store)/catalog/page.tsx
@@ -1,5 +1,5 @@
 import { ProductsCatalog } from '@widgets/ProductsCatalog'
 
-export default function Page() {
-	return <ProductsCatalog />
+export default function Page({ page = '1', limit = '12' }: { page: string, limit: string }) {
+	return <ProductsCatalog page={+page} limit={+limit} />
 }
