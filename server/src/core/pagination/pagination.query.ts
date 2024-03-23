0ead57299e016ef10f2f9f4487738a8184fc7c01
Иду в темноту <83702050+going-hi@users.noreply.github.com>
2024-03-23 23:56:14 +0300

    add @ApiProperty

--- server/src/core/pagination/pagination.query.ts
+++ server/src/core/pagination/pagination.query.ts
@@ -1,12 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsNumber, IsOptional } from 'class-validator';
 
 export class PaginationQueryDto {
+  @ApiProperty()
   @IsOptional()
   @Type(() => Number)
   @IsNumber()
   count: number = 10;
 
+  @ApiProperty()
   @IsOptional()
   @Type(() => Number)
   @IsNumber()
